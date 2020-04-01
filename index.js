36ce2c45692a6f12cade6d25c00db64c6e39f257
LejlaDolovac <Llleyla_@hotmail.com>
2020-04-01 09:36:09 +0200

    fixed backend

--- index.js
+++ index.js
@@ -1,156 +1,118 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const axios = require('axios');
-const authPassport = require('./authPassport/authRoutes');
-const passportSetup = require('./passport/passportSetup');
-const bodyParser = require('body-parser'); 
-const exphbs = require('express-handlebars');
-const nodemailer = require('nodemailer');
-const path = require('path');
-const cors = require('cors');
-const tocToHtml = require('toc-to-html');
-const { jsonToTableHtmlString } = require('json-table-converter');
-const config = require('./config');
+const express = require("express");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const authPassport = require("./authPassport/authRoutes");
+const passportSetup = require("./passport/passportSetup");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
+const path = require("path");
+const cors = require("cors");
+const tocToHtml = require("toc-to-html");
+const { jsonToTableHtmlString } = require("json-table-converter");
+const config = require("./config");
 
-
-
-let UserInformation = require('./models/Schema');
-let monthReportSchema = require('./models/monthReportSchema');
-
-
+let UserInformation = require("./models/Schema");
+let monthReportSchema = require("./models/monthReportSchema");
 
 let app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
-
-app.use('/auth', authPassport);
-
-
-
-
+app.use("/auth", authPassport);
 
 // NODEMAILER
 
 const transporter = nodemailer.createTransport({
-    service: 'hotmail',
-    auth: {
-      user: config.email,
-      pass: config.password
-    }
-  });
+  service: "hotmail",
+  auth: {
+    user: config.email,
+    pass: config.password
+  }
+});
 
 const mailOptions = {
-    from: config.email, // skapa så att den känner av vem som är inloggad
-    to: config.email,
-    subject: 'Timelog',
-    text: '{{userName.userId}}' + 'Here are your working hours. Great job!'
+  from: config.email, // skapa så att den känner av vem som är inloggad
+  to: config.email,
+  subject: "Timelog",
+  text: "{{userName.userId}}" + "Here are your working hours. Great job!"
 };
 
-
-
-
-
 // CONNECT TO MY DB
-mongoose.connect(`mongodb+srv://LejlaDolovac:guess123@cluster0-gxwz7.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true })	
-    .then(() => {	
-        console.info('Connected.')	;
-    })	
-    .catch(err => {
-        console.error(err)	;
-    });
+mongoose
+  .connect(
+    `mongodb+srv://LejlaDolovac:guess123@cluster0-gxwz7.mongodb.net/test?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.info("Connected.");
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
-    
 //ROUTES
 
-    let postGetInformation = require('./routes/postGetInformation');
-    let monthCount = require('./routes/monthCount');
-    let editPosts = require('./routes/editPosts');
+let postGetInformation = require("./routes/postGetInformation");
+let monthCount = require("./routes/monthCount");
+let editPosts = require("./routes/editPosts");
 
+app
+  .route("/postGetInformation")
+  .post(postGetInformation.post)
+  .get(postGetInformation.get);
 
-    app.route('/postGetInformation')
-    .post(postGetInformation.post)
-    .get(postGetInformation.get);
-    
-    app.route('/editPosts')
-    .post(editPosts.post);
+app.route("/postGetSpanInformation").post(postGetInformation.getSpan);
 
+app.route("/editPosts").post(editPosts.post);
 
-app.route('/monthCount')
-.post(monthCount.post);
-
-
+app.route("/monthCount").post(monthCount.post);
 
 //SLACK
 
-    app.post('/slack', async (req, res) => {
+app.post("/slack", async (req, res) => {
+  let msg = req.body.text;
+  let userName = req.body.user_name;
+  let comment = req.body.msg;
+  let date = req.body.date;
 
-        let msg = req.body.text;
-        let userName = req.body.user_name;
-        let comment = req.body.msg;
-        let date = req.body.date;
+  const data = {
+    userId: userName,
+    comment: msg,
+    date: new Date()
+  };
 
+  await UserInformation.create(data);
 
-        
+  const message = {
+    text: "Bra jobbat! Vi registrerar detta :)"
+  };
 
-          const data = {
-        
-            "userId": userName,
-            "comment": msg,
-            "date": new Date()
-        };
-
-        await UserInformation.create(data);
-
-        const message = {
-            "text": "Bra jobbat! Vi registrerar detta :)",
-            
-        };
-
-        res.send(message);
-
-    });
-
-
-
-
+  res.send(message);
+});
 
 // NODEMAILER
 
-    app.post('/mail', (req, res) =>{
+app.post("/mail", (req, res) => {
+  const userData = req.body;
+  mailOptions.text = jsonToTableHtmlString(userData);
 
-        const userData = req.body;
-        mailOptions.text = jsonToTableHtmlString(userData);
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+});
 
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-    });
+// VIEW ENGINE SETUP
 
-
-
-    
-
-  // VIEW ENGINE SETUP
-
-  app.engine('handlebars', exphbs());
-  app.set('view engine', 'handlebars');
-
-
-
-
-
-
-
-
-
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 app.listen(3000, () => {
-    console.info('Server is running on: 3000.');
+  console.info("Server is running on: 3000.");
 });
--- routes/postGetInformation.js
+++ routes/postGetInformation.js
@@ -1,25 +1,45 @@
-let UserInformation = require('../models/Schema');
+let UserInformation = require("../models/Schema");
 
-module.exports.get = async(req, res) => {
-  console.log('get');
+module.exports.get = async (req, res) => {
+  console.log("get");
 
-    try {
-        let User = await UserInformation.find({});  
-        res.status(200).send(User);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+  try {
+    let User = await UserInformation.find({});
+    res.status(200).send(User);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-module.exports.post = async(req, res, err) => {
-    console.log('post');
-    try {
-        let userData = req.body;
-        let resp = await UserInformation.create(userData);
-        res.status(200).send(resp);
-        
-    } catch(err) {
-        res.status(400).send(err.stack , 'The data wasent load');
-    }
+module.exports.post = async (req, res, err) => {
+  console.log("post");
+  try {
+    let userData = req.body;
+    let resp = await UserInformation.create(userData);
+    res.status(200).send(resp);
+  } catch (err) {
+    res.status(400).send(err.stack, "The data wasent load");
+  }
 };
 
+module.exports.getSpan = async (req, res) => {
+  console.log("getSpan");
+
+  console.log(req.body.from);
+
+  try {
+    let User = await UserInformation.find({
+      date: { $gte: req.body.from, $lt: req.body.to }
+    });
+    res.status(200).send(User);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+/*
+i FE skapa en knapp som gör GET och hämtar datumen, på detta viset ser det ut som olika boxar... 
+i BE , find({}) 
+skickar du med tex date.02 som argument och på detta settet skapar du ett filter
+
+*/
